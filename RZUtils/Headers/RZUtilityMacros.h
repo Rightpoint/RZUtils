16ff6f6cf32a04ca890112a8986bc255f73728c5
Nick Donaldson <ndonald2@gmail.com>
2013-11-01 14:27:33 -0400

    Rename headers

--- RZUtils/Headers/RZUtilityMacros.h
+++ RZUtils/Headers/RZUtilityMacros.h
@@ -1,12 +1,14 @@
 //
-//  RZUtilHeader.h
+//  RZUtilityMacros.h
 //
 //  Created by Nick Donaldson on 10/11/13.
 //  Copyright (c) 2013 Raizlabs. All rights reserved.
 //
 
-#ifndef RZUtilHeader_h
-#define RZUtilHeader_h
+//  This file contains macros for quick conversions and whatnot.
+
+#ifndef RZUtilityMacros_h
+#define RZUtilityMacros_h
 
 #import <Foundation/Foundation.h>
 
@@ -18,20 +20,5 @@
 #define RZColorFromRGB(r,g,b)       [UIColor colorWithRed:r/255.f green:g/255.f blue:b/255.f alpha:1.0]
 #define RZColorFromRGBA(r,g,b,a)    [UIColor colorWithRed:r/255.f green:g/255.f blue:b/255.f alpha:a/255.f]
 
-// Dispatch to main queue synchronously, regardless of current thread
-static inline void rz_dispatch_main_reentrant(void(^block)())
-{
-    if (block)
-    {
-        if ([NSThread isMainThread])
-        {
-            block();
-        }
-        else
-        {
-            dispatch_sync(dispatch_get_main_queue(), block);
-        }
-    }
-}
 
 #endif
