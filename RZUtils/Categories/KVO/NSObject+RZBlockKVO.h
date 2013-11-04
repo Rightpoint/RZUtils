5245813c4b68f7bf79af1124d7952494d62d9c89
Nick Donaldson <ndonald2@gmail.com>
2013-11-04 10:05:37 -0500

    Update block KVO to use observer pattern correctly

--- RZUtils/Categories/KVO/NSObject+RZBlockKVO.h
+++ RZUtils/Categories/KVO/NSObject+RZBlockKVO.h
@@ -11,11 +11,11 @@ typedef void (^RZKVOBlock)(NSDictionary *change);
 
 @interface NSObject (RZBlockKVO)
 
-// Start observing changes on an object/keypath, using a change block. Will automatically stop observing when observer is deallocated.
-- (void)rz_beginObservingObject:(NSObject *)object keyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options withBlock:(RZKVOBlock)block;
+// Add observer for changes on an object/keypath, using a change block. Will automatically remove observer when observer is deallocated.
+- (void)rz_addObserver:(NSObject *)observer forKeyPath:(NSString *)keyPath options:(NSKeyValueObservingOptions)options withBlock:(RZKVOBlock)block;
 
-// Stop observing changes on an object/keypath.
-// If keyPath is nil, will stop observing all observed keypaths on object
-- (void)rz_endObservingObject:(NSObject *)object keyPath:(NSString *)keyPath;
+// Remove observer for keypath
+// If keyPath is nil, will stop observing all observed keypaths
+- (void)rz_removeObserver:(NSObject *)observer keyPath:(NSString *)keyPath;
 
 @end
