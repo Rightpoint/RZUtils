9725c092ee2b12c67189815bde60778b78a86f78
Alex Rouse <alex.rouse@raizlabs.com>
2013-11-05 11:16:19 -0500

    Adding a cast from the RZStringToNumber - RZNumberToString to avoid a warning.

--- RZUtils/Headers/RZUtilityMacros.h
+++ RZUtils/Headers/RZUtilityMacros.h
@@ -14,8 +14,8 @@
 
 #define RZNilToEmptyString(x)   (x ? x : @"")
 #define RZNilToZeroNumber(x)    (x ? x : @0)
-#define RZStringToNumber(x)     ([x isKindOfClass:[NSNumber class]] ? x : ([x isKindOfClass:[NSString class]] ? @([x floatValue]) : nil))
-#define RZNumberToString(x)     ([x isKindOfClass:[NSString class]] ? x : ([x isKindOfClass:[NSNumber class]] ? [x stringValue] : nil))
+#define RZStringToNumber(x)     (NSNumber *)([x isKindOfClass:[NSNumber class]] ? x : ([x isKindOfClass:[NSString class]] ? @([x floatValue]) : nil))
+#define RZNumberToString(x)     (NSString *)([x isKindOfClass:[NSString class]] ? x : ([x isKindOfClass:[NSNumber class]] ? [x stringValue] : nil))
 
 #define RZColorFromRGB(r,g,b)       [UIColor colorWithRed:r/255.f green:g/255.f blue:b/255.f alpha:1.0]
 #define RZColorFromRGBA(r,g,b,a)    [UIColor colorWithRed:r/255.f green:g/255.f blue:b/255.f alpha:a/255.f]
