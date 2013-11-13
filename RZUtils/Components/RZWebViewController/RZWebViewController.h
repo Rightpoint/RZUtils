2f8efc47b6dbf763b716b297498a8501025dbb85
Alex Rouse <alex.rouse@raizlabs.com>
2013-11-13 07:53:29 -0500

    Adding a webContainerViewController to RZUtils.

---
+++ RZUtils/Components/RZWebViewController/RZWebViewController.h
@@ -0,0 +1,39 @@
+//
+//  RZWebViewController.h
+//  Raizlabs
+//
+//  Created by Alex Rouse on 11/12/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+// A simple webview for displaying local/remote HTML content.
+#import <UIKit/UIKit.h>
+
+@protocol RZWebViewControllerDelegate;
+
+@interface RZWebViewController : UIViewController
+
+@property (weak, nonatomic) id<RZWebViewControllerDelegate> delegate;
+
+// Should the webview be alloud to open additional links
+@property (nonatomic, assign) BOOL allowsWebNavigation;
+
+@property (nonatomic, strong) UIWebView *webView;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
+
+// Intialize the webview with a localFile in the bundle
+- (id)initWithLocalFileName:(NSString *)fileName;
+
+// Intialize the webview with a remote Website URL
+- (id)initWithRemoteURL:(NSURL *)webURL;
+
+
+@end
+
+
+@protocol RZWebViewControllerDelegate <NSObject>
+
+@optional
+- (void)webViewControllerDidFinishLoad:(RZWebViewController *)webVC;
+
+@end
