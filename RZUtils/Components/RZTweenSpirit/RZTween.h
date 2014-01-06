46146037d7bfa1ff5b9c4cddff35a7a544bf08b3
Nick Donaldson <ndonald2@gmail.com>
2014-01-06 12:16:08 -0500

    fix headers, add animated image view subclass

--- RZUtils/Components/RZTweenSpirit/RZTween.h
+++ RZUtils/Components/RZTweenSpirit/RZTween.h
@@ -1,9 +1,9 @@
 //
 //  RZTween.h
-//  VirginPulse
+//  Raizlabs
 //
 //  Created by Nick D on 1/3/14.
-//  Copyright (c) 2014 Virgin. All rights reserved.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- RZUtils/Components/RZTweenSpirit/RZTween.m
+++ RZUtils/Components/RZTweenSpirit/RZTween.m
@@ -1,9 +1,9 @@
 //
 //  RZTween.m
-//  VirginPulse
+//  Raizlabs
 //
 //  Created by Nick D on 1/3/14.
-//  Copyright (c) 2014 Virgin. All rights reserved.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
 //
 
 #import "RZTween.h"
