ecd1cdff34a684c1e486f1f002a2b0fe4ea65662
Connor Smith <connor.smith@raizlabs.com>
2014-05-27 10:57:46 -0400

    Added category for getting the average color of an image

---
+++ RZUtils/Categories/UIImage/UIImage+RZAverageColor.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+RZAverageColor.h
+//  UniFirst
+//
+//  Created by Connor Smith on 5/27/14.
+//  Copyright (c) 2014 Raizlabs. All rights reserved.
+//
+
+@interface UIImage (RZAverageColor)
+
+/**
+ *  Returns the average color of a UIImage instance.
+ *  @seealso @bUIColor+RZColorContrast
+ *
+ *  @return The average color of a UIImage instance.
+ */
+- (UIColor *)averageColor;
+
+@end
