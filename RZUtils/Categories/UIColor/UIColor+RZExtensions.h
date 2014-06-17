a6079e7afc216a7e91002082e810d9de69f89674
Andrew McKnight <amcknight2718@gmail.com>
2014-06-17 15:44:18 -0400

    documentation

--- RZUtils/Categories/UIColor/UIColor+RZExtensions.h
+++ RZUtils/Categories/UIColor/UIColor+RZExtensions.h
@@ -103,7 +103,7 @@
 + (UIColor *)rz_contrastForColor:(UIColor *)color;
 
 /**
- *  @return A string, prefixed with a “#”, defining this color with a hex string (not including alpha channel).
+ *  @return A string, prefixed with a “#”, followed by six lowercase hexadecimal digits. The alpha channel is ignored.
  */
 - (NSString *)rz_hexString;
 
