5f1f1c7b05fa3a7679f6acad401e69236bdcb28e
seanoshea <oshea.ie@gmail.com>
2015-02-28 17:49:27 +0000

    Trying to add a test app target.

---
+++ Tests/RZUtilsTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RZUtilsTestApp
+//
+//  Created by Sean O Shea on 2/28/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tests/RZUtilsTests/RZUtilsTestApp/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  RZUtilsTestApp
+//
+//  Created by Sean O Shea on 2/28/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
