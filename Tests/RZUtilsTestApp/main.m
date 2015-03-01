f54f79b72973e126ff1943facc4908b8ce181809
seanoshea <oshea.ie@gmail.com>
2015-03-01 09:47:50 +0000

    Adding back the test app at a different location.

---
+++ Tests/RZUtilsTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RZUtilsTestApp
+//
+//  Created by Sean O Shea on 3/1/15.
+//  Copyright (c) 2015 Raizlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
